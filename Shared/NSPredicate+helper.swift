97f5337e74c72ed78e1936b13dcf966d5bd8e7eb
sxc <qiuyequ@icloud.com>
2021-03-10 10:10:51 +0800

    fetch the notes

---
+++ Shared/NSPredicate+helper.swift
@@ -0,0 +1,16 @@
+//
+//  NSPredicate+helper.swift
+//  Slipbox
+//
+//  Created by Xiaochun Shen on 2021/3/10.
+//
+
+import Foundation
+import CoreData
+
+extension NSPredicate {
+    
+    static var all =  NSPredicate(format: "TRUEPREDICATE")
+    static var none = NSPredicate(format: "FALSEPREDICATE")
+}
+
---
+++ Shared/model/Note+helper.swift
@@ -0,0 +1,26 @@
+//
+//  Note+helper.swift
+//  Slipbox
+//
+//  Created by Xiaochun Shen on 2021/3/10.
+//
+
+import Foundation
+import CoreData
+
+extension Note {
+    convenience init(title: String, context: NSManagedObjectContext) {
+        self.init(context: context)
+        self.title = title
+        self.creationDate = Date()
+    }
+    
+    
+    static func fetch(_ predicate: NSPredicate) -> NSFetchRequest<Note> {
+        let request = NSFetchRequest<Note>(entityName: "Note")
+        request.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        
+        request.predicate = predicate
+        return request
+    }
+}
